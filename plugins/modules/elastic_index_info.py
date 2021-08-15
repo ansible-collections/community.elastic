11d734afc49b8dc91e182788082345a91344fed1
Rhys <rhys.james.campbell@googlemail.com>
2021-08-15 16:21:26 +0200

    Update elastic_index_info.py

--- plugins/modules/elastic_index_info.py
+++ plugins/modules/elastic_index_info.py
@@ -73,7 +73,6 @@ def main():
 
     module = AnsibleModule(
         argument_spec=argument_spec,
-        supports_check_mode=False,
         required_together=[['login_user', 'login_password']],
     )
 
