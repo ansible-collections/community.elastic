15c7dd24aa954d4fc19c41bae27102cb9a2c2c82
Rhys <rhyscampbell@bluewin.ch>
2026-07-23 12:23:43 +0200

    Replace deprecated imports (#130)

--- plugins/modules/elastic_cluster_settings.py
+++ plugins/modules/elastic_cluster_settings.py
@@ -57,7 +57,7 @@ RETURN = r'''
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_native
+from ansible.module_utils.common.text.converters import to_native
 
 
 from ansible_collections.community.elastic.plugins.module_utils.elastic_common import (
